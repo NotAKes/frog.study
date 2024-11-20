a7f664d7dd5d2a7134b1fab76f15bb127981788d
neakes <anton_fcsm920@mail.ru>
2024-11-20 19:16:17 +0100

    logo added

--- ui/login_pageUI.py
+++ ui/login_pageUI.py
@@ -12,11 +12,11 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 class Ui_LoginWindow(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 300)
+        Dialog.resize(513, 400)
         Dialog.setStyleSheet("QDialog{background-color:rgb(54, 57, 76);}\n"
 "QPushButton:hover{text-decoration:underline}")
         self.login_input = QtWidgets.QLineEdit(parent=Dialog)
-        self.login_input.setGeometry(QtCore.QRect(120, 40, 211, 31))
+        self.login_input.setGeometry(QtCore.QRect(120, 40, 301, 41))
         self.login_input.setStyleSheet("background:rgb(132, 153, 189);\n"
 "color:white;\n"
 "font-size:18px;")
@@ -27,7 +27,7 @@ class Ui_LoginWindow(object):
 "font-size:26px;")
         self.label.setObjectName("label")
         self.password_input = QtWidgets.QLineEdit(parent=Dialog)
-        self.password_input.setGeometry(QtCore.QRect(120, 100, 211, 31))
+        self.password_input.setGeometry(QtCore.QRect(120, 100, 301, 41))
         self.password_input.setStyleSheet("background:rgb(132, 153, 189);\n"
 "color:white;\n"
 "font-size:18px;")
@@ -38,7 +38,7 @@ class Ui_LoginWindow(object):
 "font-size:26px;")
         self.label_2.setObjectName("label_2")
         self.login_button = QtWidgets.QPushButton(parent=Dialog)
-        self.login_button.setGeometry(QtCore.QRect(220, 150, 161, 41))
+        self.login_button.setGeometry(QtCore.QRect(310, 180, 161, 51))
         self.login_button.setStyleSheet("background-color:rgb(110, 127, 158);\n"
 "word-break:break-word;\n"
 "color:white;\n"
@@ -48,7 +48,7 @@ class Ui_LoginWindow(object):
 "text-align:center;")
         self.login_button.setObjectName("login_button")
         self.pushButton = QtWidgets.QPushButton(parent=Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(220, 240, 161, 41))
+        self.pushButton.setGeometry(QtCore.QRect(310, 280, 161, 51))
         self.pushButton.setStyleSheet("background-color:rgb(110, 127, 158);\n"
 "word-break:break-word;\n"
 "color:white;\n"
@@ -58,19 +58,18 @@ class Ui_LoginWindow(object):
 "text-align:center;")
         self.pushButton.setObjectName("pushButton")
         self.error_label = QtWidgets.QLabel(parent=Dialog)
-        self.error_label.setGeometry(QtCore.QRect(20, 160, 181, 51))
+        self.error_label.setGeometry(QtCore.QRect(20, 160, 201, 91))
         self.error_label.setStyleSheet("color:orange;\n"
-"font-size:18px;\n"
+"font-size:20px;\n"
 "text-align: justify;\n"
 "justify-content: center;")
-        self.error_label.setText("")
         self.error_label.setObjectName("error_label")
         self.eye = QtWidgets.QLabel(parent=Dialog)
-        self.eye.setGeometry(QtCore.QRect(340, 90, 51, 41))
+        self.eye.setGeometry(QtCore.QRect(440, 90, 61, 51))
         self.eye.setText("")
         self.eye.setObjectName("eye")
         self.label_4 = QtWidgets.QLabel(parent=Dialog)
-        self.label_4.setGeometry(QtCore.QRect(280, 200, 41, 21))
+        self.label_4.setGeometry(QtCore.QRect(380, 240, 41, 21))
         self.label_4.setStyleSheet("color:white;\n"
 "font-size:18px;")
         self.label_4.setObjectName("label_4")
@@ -85,4 +84,6 @@ class Ui_LoginWindow(object):
         self.label_2.setText(_translate("Dialog", "Пароль"))
         self.login_button.setText(_translate("Dialog", "Войти как админ"))
         self.pushButton.setText(_translate("Dialog", "Войти как ученик"))
+        self.error_label.setText(_translate("Dialog", "Неверный логин \n"
+"или пароль"))
         self.label_4.setText(_translate("Dialog", "ИЛИ"))
